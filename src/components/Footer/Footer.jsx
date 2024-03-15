93236ff3bf1a768d2bd2e8e3933e22bf85bda0de
pushpendrasingh001 <pushpendrasingh1919@gmail.com>
2024-03-15 10:59:06 +0530

    add transition on devloper name

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,24 +4,31 @@ import Github from "../../assets/github.png"
 import Instagram from "../../assets/instagram.png"
 import LinkedIn from "../../assets/linkedin.png"
 import Logo from "../../assets/logo.png"
+
 function Footer() {
   return (
     <div className='Footer-container'>
       <hr/>
       <div className="footer">
-        <div className="social-links">       <img src={Github}alt="" />
-        <img src={Instagram} alt="" />
-        <img src={LinkedIn} alt="" />
+        <div className="social-links"> 
+        <a href="https://github.com/pushpendrasingh001"><img src={Github}alt="" /></a>
+        <a href="https://instagram.com/pushpendra.verma__"> <img src={Instagram} alt="" /></a>
+        <a  href="https://www.linkedin.com/in/pushpendrasingh001/"><img src={LinkedIn} alt="" /></a> 
         </div>
- 
-     
-      <div className="logo-f">
+        <div className="logo-f">
         <img src={Logo} alt="" />
       </div>
       </div>
+   
 <div className="blur blur-f-1"></div>
 <div className="blur blur-f-2"></div>
+
+      <p className='developer'
+    
+      >Developed By :- Pushpendra Singh </p>
+     
     </div>
+    
   )
 }
 
